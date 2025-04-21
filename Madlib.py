b5cc4b2db0081969eb17193755e63ca6c857e389
Taha <156198811+Tahaaleem1@users.noreply.github.com>
2025-04-22 00:50:55 +0500

    Update Madlib.py

--- Madlib.py
+++ Madlib.py
@@ -12,7 +12,7 @@
     },
     {
       "cell_type": "code",
-      "execution_count": Nope,
+      "execution_count": None,
       "metadata": {
         "colab": {
           "base_uri": "https://localhost:8080/"
